a104f559830175122d11f78bfa708ed372d63709
Ellepfan <Ellehp@yandex.ru>
2026-04-17 11:43:29 +0400

    добавил deploy

--- logistic/views.py
+++ logistic/views.py
@@ -24,4 +24,4 @@ class StockViewSet(ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'Это Мой Е.С. ФИНАЛ!!!'})
+    return Response({'message': 'Это Мой Е.С. ФИНАЛ!!!!!'})
